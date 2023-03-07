3e123f222beecac1bf48f9ee72ae83627fc10a2d
robertoapa <robertoaparuiz@gmail.com>
2023-03-07 21:02:18 +0100

    modified:   azarquiel/svelte.config.js

--- azarquiel/svelte.config.js
+++ azarquiel/svelte.config.js
@@ -1,24 +1,41 @@
-import adapter from '@sveltejs/adapter-auto';
 
-/** @type {import('@sveltejs/kit').Config} */
+// svelte.config.js
+import preprocess from 'svelte-preprocess';
+
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    // ...
+    prerender: {
+      // ...
+      handleMissingId: "ignore"
+    }
+  },
+  preprocess: preprocess()
 };
 
 export default config;
-// import adapter from '@sveltejs/adapter-static';
-// export default {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import adapter from '@sveltejs/adapter-auto';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
 // 	kit: {
-// 	  adapter: adapter({
-// 		// default options are shown. On some platforms
-// 		// these options are set automatically — see below
-// 		pages: 'build',
-// 		assets: 'build',
-// 		fallback: null,
-// 		precompress: false,
-// 		strict: true
-// 	  })
+// 		adapter: adapter()
 // 	}
-//   };
+// };
+
+
+// export default config;
