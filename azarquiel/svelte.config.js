8407094ba94dcd7e067083f15c610bf8fff932a5
robertoapa <robertoaparuiz@gmail.com>
2023-03-07 21:13:52 +0100

    modified:   azarquiel/package.json 	modified:   azarquiel/svelte.config.js

--- azarquiel/svelte.config.js
+++ azarquiel/svelte.config.js
@@ -2,40 +2,32 @@
 // svelte.config.js
 import preprocess from 'svelte-preprocess';
 
-const config = {
-  kit: {
-    // ...
-    prerender: {
-      // ...
-      handleMissingId: "ignore"
-    }
-  },
-  preprocess: preprocess()
-};
-
-export default config;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import adapter from '@sveltejs/adapter-auto';
-
-// /** @type {import('@sveltejs/kit').Config} */
 // const config = {
-// 	kit: {
-// 		adapter: adapter()
-// 	}
+//   kit: {
+//     // ...
+//     prerender: {
+// 		// ...
+// 		handleMissingId: "ignore"
+//     }
+
+//   },
+//   preprocess: preprocess()
 // };
 
-
 // export default config;
+
+
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			handleMissingId: "ignore"
+		}
+	}
+};
+
+
+export default config;
